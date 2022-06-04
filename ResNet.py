061f99ac8517742a4c8bca0c08187ea0e59d0288
apple <applezyh@outlook.com>
2022-06-04 19:04:51 +0800

    fix conv bug add basic conv model

--- ResNet.py
+++ ResNet.py
@@ -10,7 +10,46 @@
 import torch
 import torch.nn as nn
 
+class BasicConv(nn.Module):
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        groups=1,
+        relu=True,
+        bn=True,
+        bias=False,
+    ):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(
+            in_planes,
+            out_planes,
+            kernel_size=kernel_size,
+            stride=(stride, stride),
+            padding=padding,
+            dilation=(dilation, dilation),
+            groups=groups,
+            bias=bias,
+        )
+        self.bn = (
+            nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True)
+            if bn
+            else None
+        )
+        self.relu = nn.ReLU() if relu else None
 
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
 
 class ChannelPool(nn.Module):
     def forward(self, x):
@@ -24,7 +63,7 @@ class SpatialGate(nn.Module):
         super(SpatialGate, self).__init__()
         kernel_size = 7
         self.compress = ChannelPool()
-        self.spatial = nn.Conv2d(
+        self.spatial = BasicConv(
             2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False
         )
 
